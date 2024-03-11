221965334fb5974acf7a8c050b3b8e988ae16632
Gaurav Sablok <sablokg@gmail.com>
2024-03-11 11:28:47 +0100

    Update genome.py

--- genome.py
+++ genome.py
@@ -16,3 +16,18 @@ def genomesize(sampleG1peaks,
         knownpeaks = pd.read_csv(standardG1peaks, sep = "\t")
         estimatingmean = unknownpeaks//knownpeaks
     return estimatingmean*genomeStandard
+
+
+def C1estimates(samplecount):
+  """
+  a 1C function estimates which will take into account the 
+  1C estimates for the modelling of the values and the sequence encoding.
+  """
+        estimates = {}
+        while len(estimates.keys()) <= int(samplecount):
+              takeestimates = input("please provide the 1cestimates:")
+              takesamples = input("please provide the samples:")
+              estimates[takeestimates] = takesamples
+              if len(list(estimates.keys())) == int(samplecount):
+                    break
+        return [(int(i)*978000000)/2 for i in list(estimates.keys())]
